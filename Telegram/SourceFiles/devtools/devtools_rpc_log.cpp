d7780204ddb562c96e63b686dde25f000629a924
Sadiq <pageton@proton.me>
2026-09-07 14:16:38 +0300

    fix: add QMutex include for MSVC build

--- Telegram/SourceFiles/devtools/devtools_rpc_log.cpp
+++ Telegram/SourceFiles/devtools/devtools_rpc_log.cpp
@@ -14,6 +14,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include <QtCore/QDateTime>
 #include <QtCore/QJsonDocument>
+#include <QtCore/QMutex>
 
 #include <atomic>
 #include <deque>
